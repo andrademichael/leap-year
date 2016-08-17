3244c6c167a6f9850482cfe6bd525041446a2183
Michael Andrade <andrade.michael.e@gmail.com>
2016-08-17 14:18:49 -0700

    add frontend logic to take #leap-year's input, parse it and call function leapYear to return the boolean #result

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,10 @@ $(document).ready(function() {
   <!--Backend-->
 
   <!--Frontend-->
-
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+    $("#result").text(result);
+  });
 });
